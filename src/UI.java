7458708eba5b8e2babd42b414eba9b8d11d6f7ef
PerseusKyogre09 <kyogre.perseus09@gmail.com>
2025-02-05 16:24:36 +0530

    Initial commit

--- src/UI.java
+++ src/UI.java
@@ -1,5 +1,8 @@
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class UI {
     GameManager gm;
@@ -13,7 +16,7 @@ public class UI {
         this.gm = gm;
 
         createMainField();
-        createBackground();
+        generateSceen();
 
         window.setVisible(true);
 
@@ -37,21 +40,82 @@ public class UI {
         window.add(messageText);
     }
 
-    public void createBackground(){
+    public void createBackground(int bgNum, String bgFilename){
 
-        bgPanel[1] = new JPanel();
-        bgPanel[1].setBounds(50, 50, 700, 350);
-        bgPanel[1].setBackground(Color.blue);
-        bgPanel[1].setLayout(null);
-        window.add(bgPanel[1]);
+        bgPanel[bgNum] = new JPanel();
+        bgPanel[bgNum].setBounds(50, 50, 700, 350);
+        bgPanel[bgNum].setBackground(Color.blue);
+        bgPanel[bgNum].setLayout(null);
+        window.add(bgPanel[bgNum]);
 
-        bgLabel[1] = new JLabel();
-        bgLabel[1].setBounds(0, 0, 700, 350);
+        bgLabel[bgNum] = new JLabel();
+        bgLabel[bgNum].setBounds(0, 0, 700, 350);
 
-        ImageIcon bgIcon = new ImageIcon(getClass().getClassLoader().getResource("forest.jpeg"));
-        bgLabel[1].setIcon(bgIcon);
+        ImageIcon bgIcon = new ImageIcon(getClass().getClassLoader().getResource(bgFilename));
+        bgLabel[bgNum].setIcon(bgIcon);
 
-        bgPanel[1].add(bgLabel[1]);
+    }
+    public void createObject(int bgNum, int objx, int objy, int objWidth, int objHeight, String objFilename, String choice1Name, String choice2Name, String choice3Name) {
+        // Create New Popup Menu
+        JPopupMenu popMenu = new JPopupMenu();
+        // Create Popup Menu Items
+        JMenuItem menuItem[] = new JMenuItem[3];
+        menuItem[0] = new JMenuItem(choice1Name);
+        popMenu.add(menuItem[0]);
+
+        menuItem[1] = new JMenuItem(choice2Name);
+        popMenu.add(menuItem[1]);
+
+        menuItem[2] = new JMenuItem(choice3Name);
+        popMenu.add(menuItem[2]);
+
+        // Create Objects
+        JLabel objectLabel = new JLabel();
+        objectLabel.setBounds(objx, objy, objWidth, objHeight);
+
+        ImageIcon objectIcon = new ImageIcon(getClass().getClassLoader().getResource(objFilename));
+        objectLabel.setIcon(objectIcon);
+
+        objectLabel.addMouseListener(new MouseListener(){
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if(SwingUtilities.isRightMouseButton(e)) {
+                    popMenu.show(objectLabel, e.getX(), e.getY());
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+        bgPanel[bgNum].add(objectLabel);
+        bgPanel[bgNum].add(bgLabel[bgNum]);
+    }
+
+    public void generateSceen(){
+        //Screen 1
+        createBackground(1, "forest.png");
+        createObject(1, 475,125,200,200, "house.png", "Look", "Talk", "Rest");
+        createObject(1, 70,137,200,200, "knight.png", "Look", "Talk", "Attack");
+        createObject(1, 350,240,70,70, "chest.png", "Look", "Open", "");
+
     }
 
 }
